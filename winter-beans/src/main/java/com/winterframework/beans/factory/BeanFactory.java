40ee1461735fef7fcda36a3b7a14b8970d744918
x12986 <1092181938@qq.com>
2018-05-15 19:51:33 +0800

    描述：项目初始化 项目：winter 负责人：徐和康

---
+++ winter-beans/src/main/java/com/winterframework/beans/factory/BeanFactory.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018. HangZhou KET XuHekang, All rights reserved.
+ * <http://1092181938@qq.com/>
+ */
+
+package com.winterframework.beans.factory;
+
+/**
+ * BeanFactory
+ *
+ * @author Hekang Xu
+ * @since 2018/5/15
+ **/
+public interface BeanFactory {
+
+}
